efdcf755d99f4473f2d99b5505d62f5eead64b04
NightWng120 <treverholmes9@gmail.com>
2024-06-20 15:00:33 -0400

    fixed issues with arist name grabbing

--- newDir.sh
+++ newDir.sh
@@ -2,7 +2,8 @@
 if [ "$(ls | grep mp3)" != "" ] ; then
 	file=$(ls | grep mp3 | awk 'NR==1{print $0}')
 	album=$(exiftool "$file" | grep "Album  "| awk '{$1=$2=""; print $0}' | sed 's/ //1' | sed 's/ //1')
-	artist=$(exiftool "$file" | grep "Artist  "| awk '{$1=$2=""; print $0}' | sed 's/ //1' | sed 's/ //1')
+	# artist=$(exiftool "$file" | grep "Artist  "| awk '{$1=$2=""; print $0}' | sed 's/ //1' | sed 's/ //1')
+	artist=$(exiftool "$file" | grep "Artist  "| awk '{$1=$2=""; print $0}' | sed 's/ //1' | sed 's/ //1' | cut -d / -f 1)
 	title=$(echo "$artist - $album")
 	mkdir "$title"
 	mv *.mp3 "$title"
