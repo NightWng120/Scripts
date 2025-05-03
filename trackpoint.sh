99ac6c8127b51246e358db49455418615b3714a9
NightWng120 <treverholmes9@gmail.com>
2025-05-03 10:43:21 -0400

    added trackpad disable script

--- trackpoint.sh
+++ trackpoint.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 xinput set-prop "TPPS/2 IBM TrackPoint" $(xinput list-props "TPPS/2 IBM TrackPoint" | grep 'Accel Speed (' | awk -F"[()]" '{print $2}') 1
+xinput --disable $(xinput list | grep Synaptics | awk '{print $5}' | grep -o '[0-9]\+')
